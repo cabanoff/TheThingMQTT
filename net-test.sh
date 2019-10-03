fc42d1939e68ad656df0a46bcc1c69ebe57a940e
cabanoff <al.al.kabanov@gmail.com>
2019-10-04 00:46:18 +0200

    net-restart.sh added

---
+++ net-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+delay=10;
+noConnectionMessage="No connection, trying again in $delay seconds...";
+ConnectionMessage="We have Internet, trying again in $delay seconds...";
+
+
+echo "INTERNET TESTER: Type Ctrl+C to quit";
+rm --force /tmp/index.site
+
+while [ 1 -eq 1 ]; do
+   if [[ $(nmcli -f STATE -t g) != 'connected' ]]; then
+       echo $noConnectionMessage; 
+	service network-manager restart
+       sleep 10;
+   #else 
+       #echo $ConnectionMessage;
+   fi
+   sleep $delay;
+done
+
+exit 0;
+
