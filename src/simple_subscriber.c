e3bc8c07a41e5ebc2b4ebce49942573bc3ed31b8
cabanoff <al.al.kabanov@gmail.com>
2019-12-12 18:46:44 +0100

    publishes to two servers

--- src/simple_subscriber.c
+++ src/simple_subscriber.c
@@ -14,8 +14,15 @@
 
 #define TOPIC_IN "mqtt-kontron/lora-gatway"
 #define ADDR_IN  "localhost"
-#define ADDR_OUT "mqtt.thethings.io"
-#define TOPIC_OUT "v2/things/SCTCW0sML2fV38mioJIPzrurDldvxEGWhsBMkrBoKkg"
+#define ADDR_OUT2 "mqtt.thethings.io"
+//#define TOPIC_OUT "v2/things/GfPq9BoJgm73pGrynXafogS_6kzVBo2wWnmzGCKr4J0"
+//#define TOPIC_OUT "v2/things/1ZtGJvaiCCoVbvlliX16R7tDwh1FxYnQfQgcySsam34"
+#define TOPIC_OUT2 "v2/things/LCJzGT3QL6jucKuFcuTyBbvQzYIMunWvHUK1ZKDdfuQ"
+//
+#define ADDR_OUT "95.46.114.123"
+#define TOPIC_OUT "smartherd/gateway1"
+#define VERSION "2.003"
+
 
 
 /* --- PRIVATE VARIABLES (GLOBAL) ------------------------------------------- */
@@ -30,19 +37,26 @@ int exit_sig; /* 1 -> application terminates cleanly (shut down hardware, close 
 int quit_sig; /* 1 -> application terminates without shutting down the hardware */
 
 
-struct mqtt_client clientIn, clientOut;
+struct mqtt_client clientIn, clientOut, clientOut2;
 uint8_t sendbufIn[2048]; /* sendbuf should be large enough to hold multiple whole mqtt messages */
 uint8_t recvbufIn[1024]; /* recvbuf should be large enough any whole mqtt message expected to be received */
 uint8_t sendbufOut[2048]; /* sendbuf should be large enough to hold multiple whole mqtt messages */
 uint8_t recvbufOut[1024]; /* recvbuf should be large enough any whole mqtt message expected to be received */
-int sockfdIn;
-int sockfdOut;
+uint8_t sendbufOut2[2048]; /* sendbuf should be large enough to hold multiple whole mqtt messages */
+uint8_t recvbufOut2[1024]; /* recvbuf should be large enough any whole mqtt message expected to be received */
+
+int sockfdIn = -1;
+int sockfdOut = -1;
+int sockfdOut2 = -1;
 const char* addrIn;
 const char* addrOut;
+const char* addrOut2;
 const char* port;
 const char* topicIn;
 const char* topicOut;
+const char* topicOut2;
 pthread_t client_daemonOut;
+pthread_t client_daemonOut2;
 pthread_t client_daemonIn;
 
 /* --- PRIVATE FUNCTIONS DECLARATION ---------------------------------------- */
@@ -67,10 +81,11 @@ void subscribe_callback(void** unused, struct mqtt_response_publish *published);
 void* publish_client_refresher(void* client);
 void* subscribe_client_refresher(void* client);
 int makePublisher(void);
+int makePublisher2(void);
 /**
  * @brief Safelty closes the \p sockfd and cancels the \p client_daemon before \c exit.
  */
-void exit_example(int status, int sockfdIn, int sockfdOut, pthread_t *client_daemonIn, pthread_t *client_daemonOut);
+void exit_example(int status, int sockfdIn, int sockfdOut, int sockfdOut2, pthread_t *client_daemonIn, pthread_t *client_daemonOut,pthread_t *client_daemonOut2);
 
 int main(int argc, const char *argv[])
 {
@@ -115,14 +130,15 @@ int main(int argc, const char *argv[])
     } else {
         topicOut = TOPIC_OUT;
     }
-
+    addrOut2 = ADDR_OUT2;
+    topicOut2 = TOPIC_OUT2;
 
     /* open the non-blocking TCP socket (connecting to the broker) */
     sockfdIn = open_nb_socket(addrIn, port);
 
     if(sockfdIn == -1){
         perror("Failed to open input socket: ");
-        exit_example(EXIT_FAILURE, sockfdIn, sockfdOut, NULL, NULL);
+        exit_example(EXIT_SUCCESS, sockfdIn, sockfdOut, sockfdOut2, &client_daemonIn, NULL,NULL);
     }
 
     mqtt_init(&clientIn, sockfdIn, sendbufIn, sizeof(sendbufIn), recvbufIn, sizeof(recvbufIn), subscribe_callback);
@@ -131,20 +147,20 @@ int main(int argc, const char *argv[])
     /* check that we don't have any errors */
     if (clientIn.error != MQTT_OK) {
         fprintf(stderr, "connect subscriber error: %s\n", mqtt_error_str(clientIn.error));
-        exit_example(EXIT_FAILURE, sockfdIn,sockfdOut,NULL, NULL);
+        exit_example(EXIT_SUCCESS, sockfdIn, sockfdOut, sockfdOut2, &client_daemonIn, NULL,NULL);
     }
 
     /* start a thread to refresh the subscriber client (handle egress and ingree client traffic) */
 
     if(pthread_create(&client_daemonIn, NULL, subscribe_client_refresher, &clientIn)) {
         fprintf(stderr, "Failed to start subscriber client daemon.\n");
-        exit_example(EXIT_FAILURE, sockfdIn, sockfdOut,NULL, NULL);
+       exit_example(EXIT_SUCCESS, sockfdIn, sockfdOut, sockfdOut2, &client_daemonIn, NULL,NULL);
     }
 
     /* subscribe */
     mqtt_subscribe(&clientIn, topicIn, 0);
 
-    printf("%s listening for '%s' messages.\n", argv[0], topicIn);
+    printf("Version %s listening for '%s' messages.\n", VERSION, topicIn);
 
     /* configure signal handling */
 	sigemptyset(&sigact.sa_mask);
@@ -169,7 +185,13 @@ int main(int argc, const char *argv[])
                 mqtt_publish(&clientOut, topicOut, messageToPublish, strlen(messageToPublish), MQTT_PUBLISH_QOS_0);
                 if (clientOut.error == MQTT_OK) {
                     if(mqtt_sync(&clientOut) == MQTT_OK) sent_ok = 1;
-
+                }
+                if(makePublisher2() != -1){
+                     printf("published to test server\n");
+                     mqtt_publish(&clientOut2, topicOut2, messageToPublish, strlen(messageToPublish), MQTT_PUBLISH_QOS_0);
+                     if (clientOut.error == MQTT_OK) {
+                        if(mqtt_sync(&clientOut2) == MQTT_OK) close(sockfdOut2);
+                     }
                 }
 
             }
@@ -194,7 +216,7 @@ int main(int argc, const char *argv[])
 
     /* exit */
     //exit_example(EXIT_SUCCESS, sockfdIn, sockfdOut, &client_daemonIn, &client_daemonOut);
-    exit_example(EXIT_SUCCESS, sockfdIn, sockfdOut, &client_daemonIn, NULL);
+    exit_example(EXIT_SUCCESS, sockfdIn, sockfdOut, sockfdOut2, &client_daemonIn, NULL,NULL);
 }
 
 /* --- PRIVATE FUNCTIONS DEFINITION ----------------------------------------- */
@@ -204,7 +226,7 @@ int makePublisher(void){
     sockfdOut = open_nb_socket(addrOut, port);
 
     if(sockfdOut == -1){
-        perror("Failed to open input socket: ");
+        perror("Failed to open output socket: ");
         //exit_example(EXIT_FAILURE, sockfdIn, sockfdOut, NULL, NULL);
         return -1;
     }
@@ -231,6 +253,38 @@ int makePublisher(void){
 
 }
 
+int makePublisher2(void){
+
+    sockfdOut2 = open_nb_socket(addrOut2, port);
+
+    if(sockfdOut2 == -1){
+        perror("Failed to open output2 socket: ");
+        //exit_example(EXIT_FAILURE, sockfdIn, sockfdOut, NULL, NULL);
+        return -1;
+    }
+    mqtt_init(&clientOut2, sockfdOut2, sendbufOut2, sizeof(sendbufOut2), recvbufOut2, sizeof(recvbufOut2), publish_callback);
+    mqtt_connect(&clientOut2, "publishing_client2", NULL, NULL, 0, NULL, NULL, 0, 400);
+
+    /* check that we don't have any errors */
+    if (clientOut2.error != MQTT_OK) {
+        fprintf(stderr, "connect publisher2 error: %s\n", mqtt_error_str(clientOut2.error));
+        //exit_example(EXIT_FAILURE, sockfdIn, sockfdOut,NULL,NULL);
+        if (sockfdOut2 != -1) close(sockfdOut2);
+        return -1;
+
+    }
+    return 0;
+     /* start a thread to refresh the publisher client (handle egress and ingree client traffic) */
+//
+//    if(pthread_create(&client_daemonOut, NULL, publish_client_refresher, &clientOut)) {
+//        fprintf(stderr, "Failed to start publisher client daemon.\n");
+//        //exit_example(EXIT_FAILURE, sockfdIn, sockfdOut,NULL, NULL);
+//        if (sockfdOut != -1) close(sockfdOut);
+//        return;
+//    }
+
+}
+
 void sig_handler(int sigio) {
 	if (sigio == SIGQUIT) {
 		quit_sig = 1;
@@ -238,11 +292,13 @@ void sig_handler(int sigio) {
 		exit_sig = 1;
 	}
 }
-void exit_example(int status, int sockfdIn, int sockfdOut, pthread_t *client_daemonIn, pthread_t *client_daemonOut)
+void exit_example(int status, int sockfdIn, int sockfdOut, int sockfdOut2, pthread_t *client_daemonIn, pthread_t *client_daemonOut,pthread_t *client_daemonOut2)
 {
     if (sockfdIn != -1) close(sockfdIn);
     if (sockfdOut != -1) close(sockfdOut);
+    if (sockfdOut2 != -1) close(sockfdOut2);
     if (client_daemonIn != NULL) pthread_cancel(*client_daemonIn);
+    if (client_daemonOut != NULL) pthread_cancel(*client_daemonOut);
     if (client_daemonOut != NULL) pthread_cancel(*client_daemonOut);
     exit(status);
 }
